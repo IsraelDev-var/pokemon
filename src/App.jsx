b80d69b22423a36ea0763c7ac33b5d82efa1ebd7
Israel <israel230804vc@gmail.com>
2023-08-27 21:38:18 -0400

    creacion de la store inicialisada

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,18 @@
-import { Route, Routes } from "react-router-dom"
-import Home from "./pages/Home"
-import PokemonInfo from "./pages/PokemonInfo"
-import Pokedex from "./pages/Pokedex"
-import Error404 from "./pages/Error404"
-
-
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import PokemonInfo from "./pages/PokemonInfo";
+import Pokedex from "./pages/Pokedex";
+import Error404 from "./pages/Error404";
 
 function App() {
-  
-
   return (
-    <Routes >
-      <Route path="/" element={<Home />} /> 
-      <Route path="/pokedex" element={<Pokedex />} /> 
-      <Route path="/pokedex/:pokedexId" element={<PokemonInfo />} /> 
-      <Route path="*" element={<Error404 />} /> 
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/pokedex" element={<Pokedex />} />
+      <Route path="/pokedex/:pokedexId" element={<PokemonInfo />} />
+      <Route path="*" element={<Error404 />} />
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,28 +1,48 @@
-import Footer from "../components/Footer"
+import Footer from "../components/Footer";
 
 const Home = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const nameUser = e.target.name.value;
+    console.log(nameUser);
+  };
   return (
-    <main >
-        <section className="main h-screen grid place-items-center p-2 standar-value">
-        <seccion className="home__container min-w-[300px] max-w-[600px]">
-            <div className="home__image">
-                <img src="/images/image11.png" alt="img titulo" className="home__img bg-cover bg-center md:max-w-[660px]" />
-            </div>
-            <div className="home__info">
-                <h2 className="home__title text-red-500">¡Hola entrenador!</h2>
-                <p>Para pode comenzar, <span>dame tu nombr</span></p>
-            </div>
-            <div className="container__input ">
-                <input type="text" required placeholder="Tu nombre" id="name" className="home__input md:w-[300px] p-2 shadow-lg" />
-                <button className="home__button py-2 px-[.5rem] m bg-red-600">Comenzar</button>
-            </div>
-        </seccion>
-
+    <main>
+      <section className="main h-screen grid place-items-center p-2 standar-value">
+        <section className="home__container min-w-[300px] max-w-[600px]">
+          <div className="home__image">
+            <img
+              src="/images/image11.png"
+              alt="img titulo"
+              className="home__img bg-cover bg-center md:max-w-[660px]"
+            />
+          </div>
+          <div className="home__info">
+            <h2 className="home__title text-red-500">¡Hola entrenador!</h2>
+            <p>
+              To start, <span className="text-red-500">givr me tor name</span>
+            </p>
+          </div>
+          <form
+            onSubmit={handleSubmit}
+            className="home__form container__input "
+          >
+            <input
+              type="text"
+              required
+              autoComplete="off"
+              placeholder="Touy name"
+              id="name"
+              className="home__input md:w-[300px] p-2 shadow-lg"
+            />
+            <button className="home__button py-2 px-[.5rem] m bg-red-600">
+              start
+            </button>
+          </form>
         </section>
-        <Footer />
-
+      </section>
+      <Footer />
     </main>
-
-  )
-}
-export default Home
+  );
+};
+export default Home;
